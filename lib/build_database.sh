f2bf16f4862807bb2c406e145378f396b1430836
Christie-Rose Woodside <144174658+cwoodside1278@users.noreply.github.com>
2026-03-31 15:56:58 -0400

    Update build_database.sh to removed my print statements

--- lib/build_database.sh
+++ lib/build_database.sh
@@ -4,6 +4,7 @@ set -e
 
 pushd .. 2>&1 > /dev/null
 # nodes, names, and host are all inside new_taxdump.tar.gz. If it is not uncompressed then this code will fail.
+# Note the filepath of nodes, names, and host. 
 if [ ! -f CensuScopeDB/nodes.dmp ] || [ ! -f CensuScopeDB/names.dmp ] || [ ! -f CensuScopeDB/host.dmp ]; then
     tar -xzf CensuScopeDB/new_taxdump.tar.gz -C CensuScopeDB nodes.dmp names.dmp host.dmp
 fi
@@ -11,19 +12,13 @@ fi
 echo "Start building database: $(date)"
 ./lib/nucleotide-db.sh CensuScopeDB/ taxonomy.db
 
-#debugging line:
-#./lib/nucleotide-db.sh CensuScopeDB/debug/ taxonomy.db
-
-#echo "- Finished nucleotide-db: $(date)"
 ./lib/add-nodes.sh CensuScopeDB/nodes.dmp taxonomy.db
-#echo "- Finished add-nodes: $(date)"
+
 ./lib/add-names.sh CensuScopeDB/names.dmp taxonomy.db
-#echo "- Finished add-names: $(date)"
 
 cp taxonomy.db temp.db
 ./lib/add-hosts.sh CensuScopeDB/host.dmp temp.db
 
-#echo "- Finished add-hosts: $(date)"
 mv temp.db taxonomy.db
 
 echo "Finished building database: $(date)"
